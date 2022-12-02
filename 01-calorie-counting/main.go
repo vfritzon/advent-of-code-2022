ebd528d1667184cdc99c083a32e474a840a12253
Viktor Fritzon <viktor@tool.builders>
2022-12-03 00:19:43 +0100

    day 1, part 2

--- 01-calorie-counting/main.go
+++ 01-calorie-counting/main.go
@@ -4,14 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
 func main() {
 	fileName := "input.txt"
+
+	fmt.Println(part1(fileName))
+	fmt.Println(part2(fileName))
+}
+
+func part1(fileName string) (max int) {
 	file, _ := os.Open(fileName)
 	scanner := bufio.NewScanner(file)
-
 	maxCalories := 0
 	currentElfCalories := 0
 	for {
@@ -33,6 +39,42 @@ func main() {
 			break
 		}
 	}
+	file.Close()
 
-	fmt.Print(maxCalories)
+	return maxCalories
+}
+
+func part2(fileName string) (max int) {
+	file, _ := os.Open(fileName)
+	scanner := bufio.NewScanner(file)
+	top3 := []int{0, 0, 0}
+	currentElfCalories := 0
+	for {
+		lastItem := !scanner.Scan()
+		line := scanner.Text()
+		lastItemForElf := line == ""
+
+		if lastItem || lastItemForElf {
+			if currentElfCalories > top3[0] {
+				top3[0] = currentElfCalories
+				sort.Ints(top3)
+			}
+			currentElfCalories = 0
+		} else {
+			itemCalories, _ := strconv.Atoi(line)
+			currentElfCalories += itemCalories
+		}
+
+		if lastItem {
+			file.Close()
+			break
+		}
+	}
+
+	sum := 0
+	for _, v := range top3 {
+		sum += v
+	}
+
+	return sum
 }
