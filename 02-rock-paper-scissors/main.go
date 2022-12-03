ef1f5492461b4695b5ffb185841aa4ae4271cbf5
Viktor Fritzon <viktor@tool.builders>
2022-12-03 22:23:57 +0100

    day 2, part 2

--- 02-rock-paper-scissors/main.go
+++ 02-rock-paper-scissors/main.go
@@ -7,7 +7,10 @@ import (
 )
 
 func main() {
-	roundScore := map[string]int{
+	// A, X = rock, 1
+	// B, Y = paper, 2
+	// C, Z = scissors, 3
+	scoreMap1 := map[string]int{
 		"A X": 1 + 3,
 		"A Y": 2 + 6,
 		"A Z": 3 + 0,
@@ -19,7 +22,29 @@ func main() {
 		"C Z": 3 + 3,
 	}
 
-	file, err := os.Open("input.txt")
+	// A = rock, 1
+	// B = paper, 2
+	// C = scissors, 3
+	// X = lose
+	// Y = draw
+	// Z = win
+	scoreMap2 := map[string]int{
+		"A X": 3 + 0,
+		"A Y": 1 + 3,
+		"A Z": 2 + 6,
+		"B X": 1 + 0,
+		"B Y": 2 + 3,
+		"B Z": 3 + 6,
+		"C X": 2 + 0,
+		"C Y": 3 + 3,
+		"C Z": 1 + 6,
+	}
+	fmt.Println(calculateScore("input.txt", scoreMap1))
+	fmt.Println(calculateScore("input.txt", scoreMap2))
+}
+
+func calculateScore(fileName string, scoreMap map[string]int) int {
+	file, err := os.Open(fileName)
 	defer file.Close()
 	if err != nil {
 		panic(err)
@@ -28,8 +53,7 @@ func main() {
 	totalScore := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		totalScore += roundScore[scanner.Text()]
+		totalScore += scoreMap[scanner.Text()]
 	}
-
-	fmt.Print(totalScore)
+	return totalScore
 }
